9775872bb6d707c4b1367912369c0e0d4711419e
moi <moi@Layla.xunil.fr>
2014-01-28 15:18:12 +0100

    Init projet avec cmake (devrait compiler si Opengl + openNI installé)

---
+++ projet/build/CMakeFiles/CharA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CharA.dir/src/main.o"
+  "CharA_d.pdb"
+  "CharA_d"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CharA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
